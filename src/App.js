1e8def4395e22e93e9b7695ee33f32123837eb8b
Shubham Negi <shubham.n@trgint.com>
2021-02-18 14:43:55 +0530

    Added the webcam component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Capture from './components/capture';
+
+function App() {
+  function handleClick(){
+
+  }
+  return (
+    <div className="App">
+      <Capture />
+    </div>
+  );
+}
+
+export default App;
