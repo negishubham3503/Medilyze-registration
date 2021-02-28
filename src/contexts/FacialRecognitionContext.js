cca06c62ca73a5a1d7f977b0fd8df86518ccfa80
Sahil Mishra <sahilmishra0012@outlook.com>
2021-02-28 20:08:00 +0530

    Integrated Facial Recognition Backend API

--- src/contexts/FacialRecognitionContext.js
+++
@@ -1,6 +0,0 @@
-import * as tf from '@tensorflow/tfjs';
-
-export const loadTFModel = async () => {
-    const model = await tf.loadLayersModel('https://storage.googleapis.com/modelfr/tfjs/model.json');
-
-}
